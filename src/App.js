9eee48bd65ed532d111d45e11a8b8618bea55d34
paramthakkar23072004 <148239939+paramthakkar23072004@users.noreply.github.com>
2023-10-17 19:45:41 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,1 +1,21 @@
+import React from 'react'
+import Front from './Components/Front/Front'
+import { Route, Routes } from 'react-router-dom'
+import Login from './Components/Login/Login'
+import Signup from './Components/Signup/Signup'
+import Forgot from './Components/Forgot/Forgot'
 
+const App = () => {
+  return (
+    <div>
+      <Routes>
+        <Route path='/' Component={Front}/>
+        <Route path='/login' Component={Login}/>
+        <Route path='/signup' Component={Signup}/>
+        <Route path='/forgot' Component={Forgot}/>
+      </Routes>
+    </div>
+  )
+}
+
+export default App
